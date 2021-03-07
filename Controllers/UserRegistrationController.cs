22ec5cf29ecd8d1af681ba4b95c12554f73d8708
kicbar <bartlomiejkicior@gmail.com>
2021-03-07 22:16:12 +0100

    add user registration service

--- Controllers/UserRegistrationController.cs
+++ Controllers/UserRegistrationController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TicTacToe.Models;
+using TicTacToe.Services;
 
 namespace TicTacToe.Controllers
 {
     public class UserRegistrationController : Controller
     {
+        private readonly IUserService _userService;
+
+        public UserRegistrationController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserModel userModel)
+        {
+            await _userService.RegisterUser(userModel);
+            return Content($"User {userModel.FirstName} {userModel.LastName} correctly registered.");
         }
     }
 }
