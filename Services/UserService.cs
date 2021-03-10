0fbb9c091ea4b266a6fb4865c269998c0e66cd50
kicbar <bartlomiejkicior@gmail.com>
2021-03-10 07:15:32 +0100

    extend user services

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +9,29 @@ namespace TicTacToe.Services
 {
     public class UserService : IUserService
     {
+        private static ConcurrentBag<UserModel> _userStore;
+
+        public UserService()
+        {
+            _userStore = new ConcurrentBag<UserModel>();
+        }
+
+        public Task<UserModel> GetUserByEmail(string email)
+        {
+            return Task.FromResult(_userStore.FirstOrDefault(u => u.Email == email));
+        }
+
         public Task<bool> RegisterUser(UserModel userModel)
         {
+            _userStore.Add(userModel);
             return Task.FromResult(true);
+        }
+
+        public Task UpdateUser(UserModel userModel)
+        {
+            _userStore = new ConcurrentBag<UserModel>
+                (_userStore.Where(u => u.Email != userModel.Email)){userModel};
+            return Task.CompletedTask;
         }
     }
 }
