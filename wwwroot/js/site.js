fe71613d502a83ba135037ce46c241b88754f655
kicbar <bartlomiejkicior@gmail.com>
2021-03-15 22:35:59 +0100

    add bundle config

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+var interval;
+function EmailConfirmation(email) {
+    interval = setInterval(() => {
+    CheckEmailConfirmationStatus(email);
+    }, 5000)
+}
+function CheckEmailConfirmationStatus(email) {
+    $.get("/CheckEmailConfirmationStatus?email=" + email,
+        function (data) {
+            if (data === "OK") {
+                if (interval !== null)
+                    clearInterval(interval);
+                window.location.href = "/GameInvitation?email=" + email;
+                //alert("ok");
+            }
+        });
+}
